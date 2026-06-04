c952d3c4b298c6c96ef5fd7474d43a88859b53f0
mahdi farhadpour <farhadpour4187@gmail.com>
2026-06-04 14:28:52 +0330

    Update Manager.php
    add tsx js ts files for find new translations

--- src/Manager.php
+++ src/Manager.php
@@ -188,7 +188,7 @@ class Manager
 
         // Find all PHP + Twig files in the app folder, except for storage
         $finder = new Finder();
-        $finder->in($path)->exclude('storage')->exclude('vendor')->name('*.php')->name('*.twig')->name('*.vue')->files();
+        $finder->in($path)->exclude('storage')->exclude('vendor')->name('*.php')->name('*.twig')->name('*.vue')->name('*.js')->name('*.ts')->name('*.tsx')->files();
 
         /** @var \Symfony\Component\Finder\SplFileInfo $file */
         foreach ($finder as $file) {
